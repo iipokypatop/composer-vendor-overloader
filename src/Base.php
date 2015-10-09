5a7fc4f9bd431539eec6d680af4c6d5ce15fb6d6
p.semenyuk <p.semenyuk@mivar.ru>
2015-10-09 11:55:19 +0300

    initial commit

---
+++ src/Base.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Overloader;
+
+class Base
+{
+    public static function load(array $vendors_to_overload)
+    {
+        if (empty($vendors_to_overload)) {
+            return;
+        }
+
+        foreach ($vendors_to_overload as $vendor_name) {
+            assert(is_string($vendor_name));
+        }
+
+        $root = __DIR__ . '/../../../..';
+
+        $json = json_decode(file_get_contents($root . '/composer.json'));
+
+        $all_packages = array_keys(get_object_vars($json->require));
+
+        if (empty($all_packages)) {
+            return;
+        }
+
+        $packages_to_overload = [];
+        foreach ($all_packages as $package) {
+
+            list($vendor_name, $project_name) = explode('/', $package);
+
+            if (!empty($project_name)) {
+                if (in_array($vendor_name, $vendors_to_overload, true)) {
+                    $packages_to_overload[$package] = $project_name;
+                }
+            }
+        }
+
+        foreach ($packages_to_overload as $name) {
+            static::overLoadVendor($root, $name);
+        }
+    }
+
+    protected static function overLoadVendor($root, $name)
+    {
+        $vendor_autoload_path = "$root/../$name/vendor/autoload.php";
+
+        if (file_exists($vendor_autoload_path)) {
+            require_once $vendor_autoload_path;
+        }
+    }
+}
