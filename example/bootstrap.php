329a7b892c4b4acd90bb49168a675be40833ad48
p.semenyuk <p.semenyuk@mivar.ru>
2015-10-13 17:46:59 +0300

    example

--- example/bootstrap.php
+++ example/bootstrap.php
@@ -1,6 +1,9 @@
 <?php
 
+
 require_once __DIR__ . "/vendor/autoload.php";
+
+/** to overload autoload vendors from ./project_root/vendor/your-vendor-1 to  ./project_root/../vendor/your-vendor-1*/
 
 \Overloader\Base::load([
     'your-vendor-1',
